ac4cb7ffda729ec64729daf9d62eb18140ca81b9
Manasvi Chaturvedi <161483791+manasvichat@users.noreply.github.com>
2025-06-08 11:52:16 -0400

    Update Research.js

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -2,7 +2,7 @@ function Research() {
     return (
         <div className="research-page">
             <div className="home-container">
-                <h1>Research</h1>
+                <h1>Research & CV</h1>
                 <p>Details about your research, methodologies, and academic contributions.</p>
             </div>
         </div>
