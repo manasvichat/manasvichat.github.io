c6dd83e45c0b02e1920b8f1c22208866701c2332
Ashely <ashely@Ashelys-MacBook-Air.local>
2025-06-08 13:24:35 -0400

    add content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import manasvi from "../assets/manasvi_profile.jpg";
+import momo from "../assets/momo.jpg";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div className="home-container">
             <div className="text-column">
-                <h1><span>Hello, my name is Manasvi</span></h1>
-                <p>I am [Your Name], a PhD student in [Your Research Area].</p>
+                <h1><span>Hello, my name is Manasvi.</span></h1>
+                <p style={{textAlign: 'justify', fontWeight: 'lighter'}}>I am a Ph.D. student in Linguistics. I am broadly <Link to="/research">interested in</Link> investigating the cognitive mechanisms underlying language use. In my free time, I enjoy spending time with my cat Momo (pictured below), painting and reading.</p>
+                <div className="image-column">
+                <img src={momo} alt="Momo" style={{ width: '350px', height: 'auto', marginTop: '27px' }}/>
+            </div>
             </div>
             <div className="image-column">
                 <img src={manasvi} alt="Manasvi Chaturvedi" />
--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,9 +1,25 @@
+import manasvi_cv from "../assets/CV_chaturvedi.pdf";
+
 function Research() {
     return (
         <div className="research-page">
             <div className="home-container">
                 <h1>Research & CV</h1>
-                <p>Details about your research, methodologies, and academic contributions.</p>
+                <div className="description" style={{ padding: '0 200px' }}>
+                    <p style={{ textAlign: 'justify', fontWeight: 'lighter'  }} >
+                        <strong style={{ color: 'palevioletred' }}>My research</strong> investigates the nature of language representations in the human mind, as revealed through speech. I am interested in questions related to <strong style={{ color: 'palevioletred' }}>the nature of language representations in the mind</strong>: What kinds of processes take place during speech planning, and what is their influence on the final production? How do internal language representations change as a result of speech input from the environment (both of one's own speech and that of others')? I am ultimately interested in what these questions can tell us about <strong style={{ color: 'palevioletred' }}>the cognitive and neural mechanisms underlying human language use</strong>, and how they may recruit domain-general processes. Other than speech, I also enjoy thinking about questions related to <strong style={{ color: 'palevioletred' }}>plurality and distributivity in meaning</strong>.
+                    </p>
+                    <div className="pdf-container" style={{ marginTop: '20px' }}>
+                    <iframe 
+                        src={manasvi_cv} 
+                        width="100%" 
+                        height="600px" 
+                        style={{ border: 'none' }}
+                        title="h PDF"
+                    ></iframe>
+                </div>
+                </div>
+                
             </div>
         </div>
     );
