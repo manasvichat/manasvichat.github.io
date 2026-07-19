a91b80c855164550af023c233729899f4e53924f
Manasvi Chaturvedi <manasvichaturvedi@Manasvis-MacBook.local>
2026-07-19 11:06:44 -0400

    updated CV, and refined research description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,14 @@ function Home() {
         <div className="home-container">
             <div className="text-column">
                 <h1><span>Hello, my name is Manasvi.</span></h1>
-                <p style={{textAlign: 'justify', fontWeight: 'lighter'}}>I am a Ph.D. student in Linguistics at <a href="https://ling.yale.edu/" target="_blank" rel="noopener noreferrer">Yale University</a>. I am broadly <Link to="/research">interested in</Link> investigating the cognitive mechanisms underlying language use. In my free time, I enjoy spending time with my cat Momo (pictured below), painting and reading.</p>
+                <p style={{textAlign: 'justify', fontWeight: 'lighter'}}>
+                    I am a 5th year Ph.D. candidate in Linguistics at <a href="https://ling.yale.edu/" target="_blank" rel="noopener noreferrer">Yale University</a>.
+                    My <Link to="/research">research</Link> seeks to uncover the mechanisms underlying cognitive flexibility in speech — how speakers adapt speech production in response to the environment and to other speakers.
+                    Specifically, I am pursuing the hypothesis that internal predictions of phonetic detail — of one's own and others’ speech — play a crucial role in speech production.
+                    My research features behavior experiments, neurocomputational modeling, and neural imaging.
+                     <br/>
+                    In my free time, I enjoy spending time with my cat Momo (pictured below), painting and reading.
+                </p>
                 <div className="image-column">
                 <img src={momo} alt="Momo" style={{ width: '350px', height: 'auto', marginTop: '27px' }} loading="eager"/>
             </div>
--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,13 +1,18 @@
-import manasvi_cv from "../assets/CV_chaturvedi.pdf";
+import manasvi_cv from "../assets/CV_0715.pdf";
 
 function Research() {
     return (
         <div className="research-page">
             <div className="home-container">
-                <h1>Research & CV</h1>
+                <h1>Recent Research & CV</h1>
                 <div className="description" style={{ padding: '0 200px' }}>
                     <p style={{ textAlign: 'justify', fontWeight: 'lighter'  }} >
-                        <strong style={{ color: 'palevioletred' }}>My research</strong> investigates the nature of language representations in the human mind, as revealed through speech. I am interested in questions related to <strong style={{ color: 'palevioletred' }}>the nature of language representations in the mind</strong>: What kinds of processes take place during speech planning, and what is their influence on the final production? How do internal language representations change as a result of speech input from the environment (both of one's own speech and that of others')? I am ultimately interested in what these questions can tell us about <strong style={{ color: 'palevioletred' }}>the cognitive and neural mechanisms underlying human language use</strong>, and how they may recruit domain-general processes. Other than speech, I also enjoy thinking about questions related to <strong style={{ color: 'palevioletred' }}>plurality and distributivity in meaning</strong>.
+                        <strong style={{ color: 'palevioletred' }}>My research</strong> investigates the nature of language representations in the human mind, as revealed through speech.
+                        I am interested in questions related to <strong style={{ color: 'palevioletred' }}>the nature of language representations in the mind</strong>:
+                        What kinds of processes take place during speech planning, and what is their influence on the final production?
+                        How do internal language representations change as a result of speech input from the environment (both of one's own speech and that of others')?
+                        I am ultimately interested in what these questions can tell us about <strong style={{ color: 'palevioletred' }}>the cognitive and neural mechanisms underlying human language use</strong>, and how they may recruit domain-general processes.
+                        Other than speech, I also enjoy thinking about questions related to <strong style={{ color: 'palevioletred' }}>plurality and distributivity in meaning</strong>.
                     </p>
                     <div className="pdf-container" style={{ marginTop: '20px' }}>
                     <iframe 
